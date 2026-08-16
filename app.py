05adcdb48803ac9f86b4e41bf494ac3958898666
pallavithammisetty27-spec <pallavithammisetty27@gmail.com>
2026-08-16 14:22:43 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,39 @@
+import streamlit as st
+from streamlit_webrtc import webrtc_streamer, VideoTransformerBase
+import cv2
+from inference_sdk import InferenceHTTPClient
+
+# Roboflow Client Setup
+client = InferenceHTTPClient(
+    api_url="https://detect.roboflow.com",
+    api_key="MEE_API_KEY_IKKADA_PETTANDI" # Ikkada mee real key pettandi
+)
+
+st.set_page_config(page_title="Smart Pothole Detection", layout="wide")
+
+st.title("🚨 Smart Pothole Detection")
+st.markdown("App open ayindhi, camera lo road ni chudandi... AI automatic ga potholes ni detect chesthundi!")
+
+class PotholeTransformer(VideoTransformerBase):
+    def transform(self, frame):
+        img = frame.to_ndarray(format="bgr24")
+        
+        # Detection
+        result = client.infer(img, model_id="find-pothole-vfind-pothole-5076e-3-yolo11s-t1-logic")
+        
+        # Potholes count
+        predictions = result.get("predictions", [])
+        count = len(predictions)
+        
+        # Screen meeda Results
+        cv2.putText(img, f"Potholes Detected: {count}", (20, 50), 
+                    cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
+        
+        return img
+
+# Direct Camera Stream
+webrtc_streamer(
+    key="example",
+    video_transformer_factory=PotholeTransformer,
+    media_stream_constraints={"video": True, "audio": False}
+)
